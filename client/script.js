e1addfe5efa123ca75d0c0aae672c88069d11e1c
Clayton Hunter <claytonhunter10@gmail.com>
2023-01-12 14:18:02 -0800

    finished the code

---
+++ client/script.js
@@ -0,0 +1,37 @@
+
+
+const chatbotForm = document.getElementById("chatbot-form");
+
+document.addEventListener("DOMContentLoaded", () => {
+    chatbotForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const chatbotFormMessage = document.getElementById("chatbot-input").value;
+        fetch('http://localhost:8000/chatbot', {
+            method: 'POST',
+            body: JSON.stringify({"message":  chatbotFormMessage}),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(response => response.json())
+          .then(data => {
+            
+
+            console.log(data);
+            const newMessage = document.createElement("div");
+            newMessage.classList.add("message");
+            newMessage.innerHTML = data.text;
+
+            const removeButton = document.createElement("button");
+            removeButton.classList.add("remove-button");
+            removeButton.innerHTML = "Remove";
+            removeButton.addEventListener("click", function(e) {
+                this.parentElement.remove();
+            });
+
+            newMessage.appendChild(removeButton);
+
+            document.getElementById("chat-history").appendChild(newMessage);
+          })
+          .catch(error => console.log(error));
+        });
+});
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+app.use(express.static("client"));
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/chatbot', (req, res) => {
+    console.log(req.body);
+    const message = req.body.message;
+    console.log(message);
+
+    // extract a number from the message if there is any
+    const number = message.match(/\d+/);
+    console.log(number);
+
+    if (number) {
+        fetch(`http://numbersapi.com/${number}`)
+            .then(response => response.text())
+            .then(data => {
+                console.log(data);
+                res.json(
+                    {text: data})
+                })
+            .catch(err => 
+                res.json(
+                    {text: "Sorry I couldn't find any information about that number."})
+            );
+    } else {
+        res.json(
+            {text: "Im sorry, I didn't understand the question. Please provide a number for me to give you information about."}
+        );
+    }
+});
+
+const port = 8000;
+app.listen(port, () => {
+    console.log("Now listening on port " + port + "!");
+});
