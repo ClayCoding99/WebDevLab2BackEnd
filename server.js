9025560aa98fce165a0bd7fbea44ac0ef88df334
Clayton Hunter <claytonhunter10@gmail.com>
2023-01-13 12:01:46 -0800

    server no longer sends client side code back

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 
-app.use(express.static("client"));
+// app.use(express.static("client"));
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
